f8ba9f48e5f3a3a9e2e3d901ce2d55d4cebb8a66
evelyn-0912000 <evelyn.vdkmp@gmail.com>
2016-06-22 13:17:15 +0200

    First version parser.

---
+++ App/Parser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Parser
+{
+    static List<List<String>> readCSV(StreamReader reader)
+    {
+        List<List<String>> convertedCSV = new List<List<string>>();
+
+        while (!reader.EndOfStream)
+        {
+            String line = reader.ReadLine();
+            string[] values = line.Split(',');
+            List<String> listValues = new List<String>(values);
+            convertedCSV.Add(listValues);
+        }
+
+        return convertedCSV;
+    }
+
+    List<List<List<String>>> CVSLoader(params string[] paths)
+    {
+        List<List<List<String>>> allLists = new List<List<List<String>>>();
+
+        for (int i = 0; i < paths.Length; i++)
+        {
+            StreamReader newReader = new StreamReader(File.OpenRead(paths[i]));
+            List<List<String>> newList = readCSV(newReader);
+            allLists.Add(newList);
+        }
+
+        // StreamReader trommelReader = new StreamReader(File.OpenRead(@"C:\Users\Evelyn\Text\School\Project 4\Fietstrommels-maart-2013.csv"));
+        // StreamReader diefstalReader = new StreamReader(File.OpenRead(@"C:\Users\Evelyn\Text\School\Project 4\fietsdiefstal-rotterdam-2011-2013.csv"));
+        
+        return allLists;
+    }
+}
